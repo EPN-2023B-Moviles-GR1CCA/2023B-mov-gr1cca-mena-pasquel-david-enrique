c62daa4b5de6997a752b509f911e1b0ad635d29b
David15-tech <davpasquel@gmail.com>
2024-01-15 22:26:39 -0500

    Mockup realizado examen

---
+++ 02.1-android(examen)/app/src/main/java/com/example/gr1examendemp/BFauna.kt
@@ -0,0 +1,11 @@
+package com.example.gr1examendemp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BFauna : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b_fauna)
+    }
+}
