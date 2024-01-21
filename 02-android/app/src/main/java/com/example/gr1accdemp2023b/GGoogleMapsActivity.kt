5ff91f82b6cb0e2bcd437a9337501230a5822826
David15-tech <davpasquel@gmail.com>
2024-01-21 18:11:53 -0500

    entregablesProytecto

--- 02-android/app/src/main/java/com/example/gr1accdemp2023b/GGoogleMapsActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accdemp2023b/GGoogleMapsActivity.kt
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class GGoogleMapsActivity : AppCompatActivity() {
 
@@ -15,6 +22,7 @@ class GGoogleMapsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ggoogle_maps)
         solicitarPermisos()
+        iniciarLogicaMapa()
     }
 
     fun solicitarPermisos(){
@@ -39,4 +47,97 @@ class GGoogleMapsActivity : AppCompatActivity() {
             )
         }
     }
+
+    fun iniciarLogicaMapa() {
+        val fragmentoMapa = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        fragmentoMapa.getMapAsync { googleMap ->
+            // with(X) => if(X != null)
+            with(googleMap) {
+                mapa = googleMap
+                establecerConfiguracionMapa()
+                moverQuicentro()
+            }
+        }
+    }
+    fun establecerConfiguracionMapa(){
+        val contexto = this.applicationContext
+        with(mapa) {
+            val permisosFineLocation = ContextCompat
+                .checkSelfPermission(
+                    contexto,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            val tienePermission = permisosFineLocation == PackageManager.PERMISSION_GRANTED
+            if(tienePermission){
+                mapa.isMyLocationEnabled = true
+                uiSettings.isMyLocationButtonEnabled = true
+            }
+            uiSettings.isZoomControlsEnabled = true
+        }
+    }
+
+    fun moverQuicentro(){
+        val zoom = 17f
+        val quicentro = LatLng (
+            -0.17626734217382561, -78.47941471041042
+            )
+        val titulo = "Quicentro"
+        val MarkQuitocentro = anadirMarcador(quicentro,titulo)
+        moverCamaraConZoom(quicentro, zoom)
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String): Marker {
+        return mapa.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )!!
+    }
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f){
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng, zoom)
+        )
+    }
+
+    fun anadirPolilinea(){
+        with(mapa){
+            val poliLineaUno = mapa
+                .addPolyline(
+                    PolylineOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1759187040647396,
+                                -78.48506472421384),
+                            LatLng(-0.17632468492901104,
+                                -78.48265589308046),
+                            LatLng(-0.17746143130181483,
+                                -78.4770533307815)
+                        )
+                )
+            poliLineaUno.tag = "linea-1"
+        }
+    }
+
+    fun anadirPoligono(){
+        with(mapa){
+            val poligonoUno = mapa
+                .addPolygon(
+                    PolygonOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1759187040647396,
+                                -78.48506472421384),
+                            LatLng(-0.17632468492901104,
+                                -78.48265589308046),
+                            LatLng(-0.17746143130181483,
+                                -78.4770533307815)
+                        )
+                )
+            poligonoUno.fillColor = -0xc771c44
+            poligonoUno.tag = "poligono-2"
+        }
+    }
+
 }
