ff48dc59d6e45a3eb218a1587c6e588927197525
David15-tech <davpasquel@gmail.com>
2024-03-04 00:51:11 -0500

    diseño

--- Proyecto02/app/build.gradle.kts
+++ Proyecto02/app/build.gradle.kts
@@ -50,15 +50,20 @@ android {
 }
 
 dependencies {
+    
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +71,15 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.google.code.gson:gson:2.8.9")
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:30.1.0"))
+    // Lubrearia de Autenticacion
+    implementation("com.google.android.gms:play-services-auth:20.0.1")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    // UI libreria auth
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    // Firestore
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-analytics")
 }
--- Proyecto02/app/src/main/java/com/example/gr1comproyectofinal/MainActivity.kt
+++ Proyecto02/app/src/main/java/com/example/gr1comproyectofinal/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.gr1comproyectofinal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.os.Handler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,37 +12,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.gr1comproyectofinal.ui.theme.Gr1comProyectoFinalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContent {
-            Gr1comProyectoFinalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+        setContentView(R.layout.activity_main)
+
+        Thread.sleep(2500)
+
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+        finish()
+
+
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Gr1comProyectoFinalTheme {
-        Greeting("Android")
-    }
-}
